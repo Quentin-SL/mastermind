aba379dc30e758473f6fbafaa83513c90fb34d74
Quentin SL <56533657+Quentin-SL@users.noreply.github.com>
2020-06-21 10:33:18 +0400

    Button couleurs en cous

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ Button(ROOT, text = "Erase the line", command = lambda : game.backspace_row()).p
 def define_number() :
     array = []
     for i in range(10) :
-        array.append(Canvas(ROOT, background = "#B2C2BB"))
+        array.append(Canvas(ROOT, background = "#B2C2BB",highlightthickness= 0))
         array[i].place(relx = 0.1, y = 100 + 52 * i, height = 50, width = 50)
         array[i].create_text(25, 25, text = str(game.board_game[i][0]))
     return array
@@ -29,7 +29,7 @@ def define_color() :
     for i in range(10) :
         array.append([])
         for j in range(4) :
-            array[i].append(Canvas(ROOT, background = "#B2C2BB"))
+            array[i].append(Canvas(ROOT, background = "#B2C2BB",highlightthickness= 0))
             array[i][j].place(relx = 0.135 + 0.035 * j, y = 100 + 52 * i, height = 50, width = 50)
             array[i][j].create_oval(25+2,25+2,25-2,25-2, fill = "#000000")
     return array
@@ -40,33 +40,55 @@ def define_hint() :
         array.append([])
         for j in range(5) :
             if j == 0:
-                array[i].append(Canvas(ROOT, background = "#B2C2BB"))
+                array[i].append(Canvas(ROOT, background = "#B2C2BB",highlightthickness= 0))
                 array[i][j].place(relx = 0.2755, y = 100 + 52 * i, height = 50, width = 50)
             if j == 1:
-                array[i].append(Canvas(array[i][0], background = "#B2C2BB"))
+                array[i].append(Canvas(array[i][0], background = "#B2C2BB",highlightthickness= 0))
                 array[i][j].place(x = 0, y = 0, height = 25, width = 25)
                 array[i][j].create_oval(12.5+5,12.5+5,12.5-5,12.5-5, fill = "black")
             if j == 2:
-                array[i].append(Canvas(array[i][0], background = "#B2C2BB"))
+                array[i].append(Canvas(array[i][0], background = "#B2C2BB",highlightthickness= 0))
                 array[i][j].place(x = 0, y = 25, height = 25, width = 25)
                 array[i][j].create_oval(12.5+5,12.5+5,12.5-5,12.5-5, fill = "black")
             if j == 3:
-                array[i].append(Canvas(array[i][0], background = "#B2C2BB"))
+                array[i].append(Canvas(array[i][0], background = "#B2C2BB",highlightthickness= 0))
                 array[i][j].place(x = 25, y = 0, height = 25, width = 25)
                 array[i][j].create_oval(12.5+5,12.5+5,12.5-5,12.5-5, fill = "black")
             if j == 4:
-                array[i].append(Canvas(array[i][0], background = "#B2C2BB"))
+                array[i].append(Canvas(array[i][0], background = "#B2C2BB",highlightthickness= 0))
                 array[i][j].place(x = 25, y = 25, height = 25, width = 25)
                 array[i][j].create_oval(12.5+5,12.5+5,12.5-5,12.5-5, fill = "black")
     return array
 
+def button_color():
+    array = []
+    for i in range(6) :
+        array.append([])
+        array[i].append(Canvas(ROOT, background = "#050A02",highlightthickness= 0))
+        array[i].place(relx = 0.1, y = 37.5, height = 50, width = 50)
+        array[i].create_oval(25+10,25+10,25-10,25-10, fill = "red")
+        array[i].bind('<Button-1>', lambda event: test_switch_color(event,color,test_button))
+        #if i == 0 :
+            #buttons[i].create_oval(fill = "red")    
+        #if i == 1 :
+            
+        #if i == 2 :
+            
+        #if i == 3 :
+            
+        #if i == 4 :
+               
+        #if i == 5 :
+    return array[i]
+
 number = define_number()
 color = define_color()
 hint = define_hint()
+buttons = button_color()
 
 color[0][0].create_oval(25+10,25+10,25-10,25-10, fill = "red")
 
-def test_switch_color(color,test_button):
+def test_switch_color(event,color,test_button):
     color[0][0].delete("all") # detruit toute les formes 
     color[game.actual_pos-1][0].create_oval(25+2,25+2,25-2,25-2, fill = "#000000")
     test_button.destroy() #disparait le button
@@ -75,6 +97,10 @@ def test_switch_color(color,test_button):
 test_button = Button(ROOT, text = "test_couleur", command = lambda : test_switch_color(color,test_button))
 test_button.place(relx = 0.45, rely = 0.6)
 
+#red_button = Canvas(ROOT, background = "#050A02",highlightthickness= 0)
+#red_button.place(relx = 0.1, y = 37.5, height = 50, width = 50)
+#red_button.create_oval(25+10,25+10,25-10,25-10, fill = "red")
+#red_button.bind('<Button-1>', lambda event: test_switch_color(event,color,test_button))
 
 ROOT.mainloop()
 
