6b75f06e7997f4568d9458f331f8e937f96a7f55
atzisb-kozak <306156@supinfo.com>
2020-06-22 14:32:30 +0400

    ajout du bloc reponse + modif du bouton test ligne

--- game.py
+++ game.py
@@ -14,13 +14,14 @@ ROOT.configure(background = '#050A02')
 ROOT.title("Mastermind")
 ROOT.geometry('%dx%d' %(ws,hs)) 
 
-Button(ROOT, text = "Test the line", command = lambda : callback.test_row(hint_block)).place(relx = 0.45, rely = 0.3)
+Button(ROOT, text = "Test the line", command = lambda : callback.victory_condition(hint_block,answer_block)).place(relx = 0.45, rely = 0.3)
 Button(ROOT, text = "Erase the line", command = lambda : callback.erase_row(color_block)).place(relx = 0.45, rely = 0.4)
 
 number_block = callback.define_number(ROOT)
 color_block = callback.define_color(ROOT)
 hint_block = callback.define_hint(ROOT)
 buttons_block = callback.button_color(ROOT,color_block)
+answer_block = callback.define_answer(ROOT)
 
 game.push_hidden_random()
 
