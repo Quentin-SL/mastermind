a0a76fe5afc3d1e3efe9946a903572ae11372419
atzisb-kozak <306156@supinfo.com>
2020-06-23 21:04:48 +0400

    réadaptation + remove TODO

--- game.py
+++ game.py
@@ -14,7 +14,7 @@ ROOT.configure(background = '#050A02')
 ROOT.title("Mastermind")
 ROOT.geometry('%dx%d' %(ws,hs)) 
 
-Button(ROOT, text = "Test the line", command = lambda : callback.victory_condition(hint_block,answer_block,ROOT)).place(relx = 0.45, rely = 0.3)
+Button(ROOT, text = "Test the line", command = lambda : callback.victory_condition(hint_block,answer_block,color_block,ROOT)).place(relx = 0.45, rely = 0.3)
 Button(ROOT, text = "Erase the line", command = lambda : callback.erase_row(color_block)).place(relx = 0.45, rely = 0.4)
 
 number_block = callback.define_number(ROOT)
@@ -23,18 +23,6 @@ hint_block = callback.define_hint(ROOT)
 buttons_block = callback.button_color(ROOT,color_block)
 answer_block = callback.define_answer(ROOT)
 
-#game.push_hidden_random()
-
-#def victory():#
-#    VICTORY = Toplevel()
-#   VICTORY.title("Victoire")
-#    VICTORY.geometry("800x600")
-#      VICTORY.transient(ROOT) # reduction impossible du pop up
-#    VICTORY.grab_set() # freeze la fenetre de jeu
-#    ROOT.wait_window(VICTORY) # freeze la mainloop du jeu
+game.push_hidden_random()
 
 ROOT.mainloop()
-
-""" TODO
-fonction callback de victoire et de defaite 
-"""
